c40e47d70c21615db344b25f75cf9ff04d63ff74
Tore Hanssen <tk.hanssen@gmail.com>
2012-11-16 21:25:50 -0800

    initial voteservlet

--- src/com/uwlighthouse/photocontest/server/VoteServlet.java
+++ src/com/uwlighthouse/photocontest/server/VoteServlet.java
@@ -4,4 +4,11 @@ import javax.servlet.http.HttpServlet;
 
 public class VoteServlet extends HttpServlet {
 	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+		getParameterValues("facebook_id");
+		getParameterValues("photo_id");
+		
+	}
+	
 }
